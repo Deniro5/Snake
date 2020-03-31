ef83bbba07778cad96cc81f2cd8b0c4b311632ae
USER <user@USERs-MacBook-Pro.local>
2020-03-31 00:07:54 -0400

    Updates

---
+++ precache-manifest.e5535ac8fccc1f4bf6520e4c11cfff27.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c26080d8a0b03b90aa45",
+    "url": "/Snake/./static/js/runtime~main.c26080d8.js"
+  },
+  {
+    "revision": "88755fe1303b4755b2bd",
+    "url": "/Snake/./static/js/main.88755fe1.chunk.js"
+  },
+  {
+    "revision": "1bab2d001fb63d40cead",
+    "url": "/Snake/./static/js/1.1bab2d00.chunk.js"
+  },
+  {
+    "revision": "88755fe1303b4755b2bd",
+    "url": "/Snake/./static/css/main.75e6e497.chunk.css"
+  },
+  {
+    "revision": "c4676cafcd04e4a9958f710fcbe04187",
+    "url": "/Snake/./index.html"
+  }
+];
